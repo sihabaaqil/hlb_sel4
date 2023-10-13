c3dc5f015eb8e9e5706054f4e5889e99e43b42c1
aaqil <taaqil@hlbb.hongleong.com.my>
2023-10-13 16:32:24 +0800

    Test

---
+++ Appium_selenium/src/main/java/demo/Test/IC/Generate_IC.java
@@ -0,0 +1,19 @@
+package demo.Test.IC;
+
+public class Generate_IC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(generate_IC());
+	}
+
+	public void generate_Name() {
+
+	}
+
+	public static long generate_IC() {
+		long ic = 830504000673L;
+		return ic;
+	}
+
+}
---
+++ Appium_selenium/src/main/java/tests/Baseclass.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Baseclass {
+
+	@BeforeTest
+	public void setup() {
+
+	}
+
+	@AfterTest
+	public void teardown() {
+
+	}
+
+}
