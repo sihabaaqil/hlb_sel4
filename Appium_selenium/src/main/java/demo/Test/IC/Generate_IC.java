dcea648bd8ccccf8594b0844b886291893f35264
aaqil <taaqil@hlbb.hongleong.com.my>
2023-12-14 10:55:23 +0800

    Pattern

--- Appium_selenium/src/main/java/demo/Test/IC/Generate_IC.java
+++ Appium_selenium/src/main/java/demo/Test/IC/Generate_IC.java
@@ -4,7 +4,7 @@ public class Generate_IC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(generate_IC());
+		System.out.println(generate2_IC());
 	}
 
 	public void generate_Name() {
@@ -13,6 +13,11 @@ public class Generate_IC {
 
 	public static long generate_IC() {
 		long ic = 830504000673L;
+		return ic;
+	}
+	
+	public static long generate2_IC() {
+		long ic = 8305040006731234567L;
 		return ic;
 	}
 
